b951af44d901bc53ac2e7c4679e484056c4e59b7
SPK80 <psukonkin@gmail.com>
2021-09-29 13:05:43 +0300

    refac AppsView.Update

--- AppsBar/AppsView.cs
+++ AppsBar/AppsView.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows.Forms;
 using System.Linq;
+using System;
 
 namespace AppsBar
 {
@@ -24,33 +25,56 @@ namespace AppsBar
 			{
 				return this.MainWindowTitle == "" ? "[Пусто]" : this.MainWindowTitle;
 			}
+
 		}
 		public AppsView()
 		{
 			this.View = View.List;
 		}
 
+		private IEnumerable<AppData> appsData
+		{
+			get => this.Items.
+			Cast<ListViewItem>().
+			Select(it => (it.Tag as AppData));
+		}
+
+		private AppData itemAppData(ListViewItem item)
+		{
+			return item.Tag as AppData;
+		}
+
 		public void Update(Process[] processes)
 		{
-
-			var selId = this.SelectedItems.
-			Cast<ListViewItem>().
-			Select(it => (it.Tag as AppData).Id).
-			ToArray();
-
-			this.BeginUpdate();
-			this.Items.Clear();
-			foreach (Process process in processes)
+			//add App if found new process
+			foreach (var process in processes)
 			{
-				var it = new ListViewItem();
-				AppData appData = (AppData)(it.Tag = new AppData(process));
-				it.Text = appData.ToString();
-				if (selId.Contains(appData.Id))
-					it.Selected = true;
-				this.Items.Add(it);
-
+				try
+				{
+					//var sameAppData = 
+					this.appsData.First(ad => ad.Id == process.Id);
+				}
+				catch (InvalidOperationException)
+				{
+					var newAppData = new AppData(process);
+					var newItem = new ListViewItem(newAppData.ToString());
+					newItem.Tag = newAppData;
+					this.Items.Add(newItem);
+				}
 			}
-			this.EndUpdate();
+
+			//find and remove killed Apps
+			foreach (ListViewItem item in this.Items)
+			{
+				try
+				{
+					processes.First(p => p.Id == itemAppData(item).Id);
+				}
+				catch (InvalidOperationException)
+				{
+					this.Items.Remove(item);
+				}
+			}
 
 		}
 
